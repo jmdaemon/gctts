ff5c6dc5c30024f8550d4b2afee5ac82dbb156d7
Joseph Diza <josephm.diza@gmail.com>
2022-09-20 23:12:42 -0700

    Fix bug where sound_directories was not an optional config option

--- tts.py
+++ tts.py
@@ -102,30 +102,35 @@ def main():
     token = cfg['config']['token'] # Note that this assumes there is a [config] token variable
 
     # If the sound already exists in our configured sound_directories,
-    soundsfp: list[str] = cfg['config']['sound_directories']
-    sounds = []
-    for fp in soundsfp:
-        sounds.append(collect_files(expand(fp)))
+    if cfg['config'].__contains__('sound_directories'):
+        soundsfp: list[str] = cfg['config']['sound_directories']
+        sounds = []
+        for fp in soundsfp:
+            sounds.append(collect_files(expand(fp)))
 
-    logger.info('Found Sounds:')
-    logger.debug(sounds)
+        logger.info('Found Sounds:')
+        logger.debug(sounds)
 
-    # Then only display the path to it, and exit
-    for sound_dir in sounds:
-        for sp in sound_dir:
-            if pathlib.Path(expand(sp)).stem == inp:
-                print(sp)
-                sys.exit(0)
+        # Then only display the path to it, and exit
+        for sound_dir in sounds:
+            for sp in sound_dir:
+                if pathlib.Path(expand(sp)).stem == inp:
+                    print(sp)
+                    sys.exit(0)
 
     # Send TTS API reuqest
+
+    # Create json body
     json_request = create_json_input(inp, voice, model)
     logger.info(f'Request JSON')
     logger.debug(json_request)
 
+    # Create headers
     request_headers = create_header(token)
     logger.info(f'Request Headers')
     logger.debug(request_headers)
 
+    # Send request
     r = requests.post(GOOGLE_APIS_TTS_URL, json=json_request, headers=request_headers)
     json_response =json.loads(r.content)
     logger.info(f'Response')
