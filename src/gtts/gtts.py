e45f4fc50445de1838041312570169145360b9ff
Joseph Diza <josephm.diza@gmail.com>
2022-10-13 05:54:38 -0700

    Add option to execute re-encode output file

--- src/gtts/gtts.py
+++ src/gtts/gtts.py
@@ -2,6 +2,7 @@
 from gtts.cli import build_cli
 from gtts.tts import (
     CONFIG,
+    is_win,
     CHARSET,
     get_cfg,
     setup_logger,
@@ -9,7 +10,7 @@ from gtts.tts import (
 )
 
 # Standard Library
-import sys, os
+import sys, os, subprocess
 
 # Third Party Libraries
 import requests
@@ -26,12 +27,14 @@ GOOGLE_TTS_QUERY = ('{tts_url}'
 def main():
     # Parse command line arguments
     parser = build_cli()
+    parser.add_argument('-r', '--re-encode', action='store_true', help='Specify the voice to use')
     args = parser.parse_args()
 
     inp = args.input
     output = args.output if args.output else f'{inp}.wav'
     voice = args.language_code if args.language_code else 'ja-JP'
     verbose = args.verbose
+    reencode = args.re_encode
 
     setup_logger(verbose)
     logger.debug(f'input: {inp}')
@@ -58,3 +61,14 @@ def main():
     # Write file to disk
     with open(output, 'wb') as f:
         f.write(r.content)
+
+    if reencode:
+        output_file = f'{output}-test.wav'
+        if is_win:
+            subprocess.run([f'msound.ps1', f'-i {output}', f'-o {output_file}'], stdout=sys.stdout)
+        else:
+            subprocess.run([f'msound', output, output_file], stdout=sys.stdout)
+
+        # Replace old file
+        os.remove(output)
+        os.rename(output_file, output)
